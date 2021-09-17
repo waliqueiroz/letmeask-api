d78a37862a863dc8f12902319eb54790e587dc14
Wali Queiroz <wali.santos@supermercadonow.com>
2021-09-17 07:58:34 -0300

    chore: authenticate room routes

--- internal/infrastructure/http/fiber/routes/room.go
+++ internal/infrastructure/http/fiber/routes/room.go
@@ -6,12 +6,12 @@ import (
 )
 
 func SetupRoomRoutes(router fiber.Router, authMiddleware fiber.Handler, roomController *controllers.RoomController) {
-	router.Post("/rooms", roomController.Create)
+	router.Post("/rooms", authMiddleware, roomController.Create)
 	router.Get("/rooms/:roomID", roomController.FindByID)
-	router.Delete("/rooms/:roomID", roomController.EndRoom)
-	router.Post("/rooms/:roomID/questions", roomController.CreateQuestion)
-	router.Post("/rooms/:roomID/questions/:questionID/likes", roomController.LikeQuestion)
-	router.Delete("/rooms/:roomID/questions/:questionID/likes/:likeID", roomController.DeslikeQuestion)
-	router.Patch("/rooms/:roomID/questions/:questionID", roomController.UpdateQuestion)
-	router.Delete("/rooms/:roomID/questions/:questionID", roomController.DeleteQuestion)
+	router.Delete("/rooms/:roomID", authMiddleware, roomController.EndRoom)
+	router.Post("/rooms/:roomID/questions", authMiddleware, roomController.CreateQuestion)
+	router.Post("/rooms/:roomID/questions/:questionID/likes", authMiddleware, roomController.LikeQuestion)
+	router.Delete("/rooms/:roomID/questions/:questionID/likes/:likeID", authMiddleware, roomController.DeslikeQuestion)
+	router.Patch("/rooms/:roomID/questions/:questionID", authMiddleware, roomController.UpdateQuestion)
+	router.Delete("/rooms/:roomID/questions/:questionID", authMiddleware, roomController.DeleteQuestion)
 }
