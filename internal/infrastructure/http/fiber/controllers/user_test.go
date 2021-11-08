6b0a6e70e29d863b771fbf8163cd255c44a4f5df
Wali Queiroz <wsantos077@gmail.com>
2021-11-08 08:21:51 -0300

    test: TestCreateUser

--- internal/infrastructure/http/fiber/controllers/user_test.go
+++ internal/infrastructure/http/fiber/controllers/user_test.go
@@ -1,6 +1,7 @@
 package controllers_test
 
 import (
+	"bytes"
 	"encoding/json"
 	"io/ioutil"
 	"net/http/httptest"
@@ -8,6 +9,7 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 	"github.com/waliqueiroz/letmeask-api/internal/application/services/mocks"
 	"github.com/waliqueiroz/letmeask-api/internal/domain/entities"
 	"github.com/waliqueiroz/letmeask-api/internal/infrastructure/http/fiber/controllers"
@@ -74,4 +76,88 @@ func TestUserIndex(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestCreateUser(t *testing.T) {
+	createUserRequestSerialized, _ := ioutil.ReadFile("../../../../../test/resources/create_user_request.json")
+	createUserRequestIncompleteSerialized, _ := ioutil.ReadFile("../../../../../test/resources/create_user_request_incomplete.json")
+	userSerialized, _ := ioutil.ReadFile("../../../../../test/resources/user.json")
+
+	var user entities.User
+	json.Unmarshal(userSerialized, &user)
+
+	tests := []struct {
+		name                 string
+		input                *bytes.Buffer
+		expectedCreateResult entities.User
+		expectedCreateCalls  int
+		expectedCreateError  error
+		expectedStatusCode   int
+	}{
+		{
+			name:                 "Create user",
+			input:                bytes.NewBuffer(createUserRequestSerialized),
+			expectedCreateResult: user,
+			expectedCreateError:  nil,
+			expectedCreateCalls:  1,
+			expectedStatusCode:   fiber.StatusCreated,
+		},
+		{
+			name:                 "Validation error while creating user",
+			input:                bytes.NewBuffer(createUserRequestIncompleteSerialized),
+			expectedCreateResult: entities.User{},
+			expectedCreateError:  nil,
+			expectedCreateCalls:  0,
+			expectedStatusCode:   fiber.StatusUnprocessableEntity,
+		},
+		{
+			name:                 "Unmarshal error while creating user",
+			input:                bytes.NewBuffer(nil),
+			expectedCreateResult: entities.User{},
+			expectedCreateError:  nil,
+			expectedCreateCalls:  0,
+			expectedStatusCode:   fiber.StatusBadRequest,
+		},
+		{
+			name:                 "Error while creating user",
+			input:                bytes.NewBuffer(createUserRequestSerialized),
+			expectedCreateResult: entities.User{},
+			expectedCreateError:  assert.AnError,
+			expectedCreateCalls:  1,
+			expectedStatusCode:   fiber.StatusInternalServerError,
+		},
+	}
+
+	validationProvider := goplayground.NewGoPlaygroundValidatorProvider()
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			userServiceMock := mocks.NewUserServiceMock()
+			userServiceMock.On("Create", mock.AnythingOfType("entities.User")).Return(test.expectedCreateResult, test.expectedCreateError)
+
+			userController := controllers.NewUserController(userServiceMock, validationProvider)
+
+			app := fiber.New()
+
+			routes.SetupUserRoutes(app, func(c *fiber.Ctx) error { return c.Next() }, userController)
+
+			req := httptest.NewRequest(fiber.MethodPost, routes.CREATE_USER_ROUTE, test.input)
+			req.Header.Set("Content-Type", "application/json")
+
+			response, _ := app.Test(req)
+
+			assert.Equal(t, test.expectedStatusCode, response.StatusCode)
+
+			userServiceMock.AssertNumberOfCalls(t, "Create", test.expectedCreateCalls)
+
+			if response.StatusCode == fiber.StatusOK {
+				body, _ := ioutil.ReadAll(response.Body)
+				var user entities.User
+				json.Unmarshal(body, &user)
+
+				assert.Equal(t, test.expectedCreateCalls, user)
+			}
+		})
+	}
+
 }
