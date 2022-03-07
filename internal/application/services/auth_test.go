1aa77222bbf8688e42dc6ba30aed42ba746306dd
Wali Queiroz <wali.santos@supermercadonow.com>
2022-03-07 10:45:38 -0300

    test: login function

--- internal/application/services/auth_test.go
+++ internal/application/services/auth_test.go
@@ -2,12 +2,14 @@ package services_test
 
 import (
 	"encoding/json"
+	"errors"
 	"io/ioutil"
 
 	"github.com/golang/mock/gomock"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"github.com/waliqueiroz/letmeask-api/internal/application/dtos"
+	application "github.com/waliqueiroz/letmeask-api/internal/application/errors"
 	"github.com/waliqueiroz/letmeask-api/internal/application/services"
 	"github.com/waliqueiroz/letmeask-api/internal/domain/entities"
 	authMocks "github.com/waliqueiroz/letmeask-api/internal/infrastructure/authentication/mocks"
@@ -79,6 +81,122 @@ var _ = Describe("Auth", func() {
 
 			It("error should be nil", func() {
 				Expect(authError).Should(BeNil())
+			})
+
+			AfterEach(func() {
+				mockCtrl.Finish()
+			})
+		})
+
+		When("an error occurs while finding user by email", func() {
+			BeforeEach(func() {
+				credentialsSerialized, err := ioutil.ReadFile("../../../test/resources/credentials.json")
+				Expect(err).NotTo(HaveOccurred())
+
+				err = json.Unmarshal(credentialsSerialized, &credentials)
+				Expect(err).NotTo(HaveOccurred())
+
+				mockCtrl = gomock.NewController(GinkgoT())
+
+				mockUserRepository := repositoriesMocks.NewMockUserRepository(mockCtrl)
+				mockUserRepository.EXPECT().FindByEmail(credentials.Email).Return(entities.User{}, errors.New("an error")).Times(1)
+
+				mockSecurityProvider := securityMocks.NewMockSecurityProvider(mockCtrl)
+
+				mockAuthenticator := authMocks.NewMockAuthenticator(mockCtrl)
+
+				authService = services.NewAuthService(mockUserRepository, mockSecurityProvider, mockAuthenticator)
+			})
+
+			It("result should be an empty struct", func() {
+				Expect(result).To(Equal(dtos.AuthDTO{}))
+			})
+
+			It("error should be an unauthorized error", func() {
+				Expect(authError).To(Equal(application.NewUnauthorizedError("credenciais inválidas")))
+			})
+
+			AfterEach(func() {
+				mockCtrl.Finish()
+			})
+		})
+
+		When("an error occurs while verifying password", func() {
+			BeforeEach(func() {
+				credentialsSerialized, err := ioutil.ReadFile("../../../test/resources/credentials.json")
+				Expect(err).NotTo(HaveOccurred())
+
+				expectedUserSerialized, err := ioutil.ReadFile("../../../test/resources/full_user.json")
+				Expect(err).NotTo(HaveOccurred())
+
+				err = json.Unmarshal(credentialsSerialized, &credentials)
+				Expect(err).NotTo(HaveOccurred())
+
+				var expectedUser entities.User
+				err = json.Unmarshal(expectedUserSerialized, &expectedUser)
+				Expect(err).NotTo(HaveOccurred())
+
+				mockCtrl = gomock.NewController(GinkgoT())
+
+				mockUserRepository := repositoriesMocks.NewMockUserRepository(mockCtrl)
+				mockUserRepository.EXPECT().FindByEmail(credentials.Email).Return(expectedUser, nil).Times(1)
+
+				mockSecurityProvider := securityMocks.NewMockSecurityProvider(mockCtrl)
+				mockSecurityProvider.EXPECT().Verify(expectedUser.Password, credentials.Password).Return(errors.New("an error")).Times(1)
+
+				mockAuthenticator := authMocks.NewMockAuthenticator(mockCtrl)
+
+				authService = services.NewAuthService(mockUserRepository, mockSecurityProvider, mockAuthenticator)
+			})
+
+			It("result should be an empty struct", func() {
+				Expect(result).To(Equal(dtos.AuthDTO{}))
+			})
+
+			It("error should an unauthorized error", func() {
+				Expect(authError).To(Equal(application.NewUnauthorizedError("credenciais inválidas")))
+			})
+
+			AfterEach(func() {
+				mockCtrl.Finish()
+			})
+		})
+
+		When("an error occurs while creating token", func() {
+			BeforeEach(func() {
+				credentialsSerialized, err := ioutil.ReadFile("../../../test/resources/credentials.json")
+				Expect(err).NotTo(HaveOccurred())
+
+				expectedUserSerialized, err := ioutil.ReadFile("../../../test/resources/full_user.json")
+				Expect(err).NotTo(HaveOccurred())
+
+				err = json.Unmarshal(credentialsSerialized, &credentials)
+				Expect(err).NotTo(HaveOccurred())
+
+				var expectedUser entities.User
+				err = json.Unmarshal(expectedUserSerialized, &expectedUser)
+				Expect(err).NotTo(HaveOccurred())
+
+				mockCtrl = gomock.NewController(GinkgoT())
+
+				mockUserRepository := repositoriesMocks.NewMockUserRepository(mockCtrl)
+				mockUserRepository.EXPECT().FindByEmail(credentials.Email).Return(expectedUser, nil).Times(1)
+
+				mockSecurityProvider := securityMocks.NewMockSecurityProvider(mockCtrl)
+				mockSecurityProvider.EXPECT().Verify(expectedUser.Password, credentials.Password).Return(nil).Times(1)
+
+				mockAuthenticator := authMocks.NewMockAuthenticator(mockCtrl)
+				mockAuthenticator.EXPECT().CreateToken(expectedUser.ID, gomock.Any()).Return("", errors.New("an error")).Times(1)
+
+				authService = services.NewAuthService(mockUserRepository, mockSecurityProvider, mockAuthenticator)
+			})
+
+			It("result should be an empty struct", func() {
+				Expect(result).To(Equal(dtos.AuthDTO{}))
+			})
+
+			It("error should an unauthorized error", func() {
+				Expect(authError).To(Equal(errors.New("an error")))
 			})
 
 			AfterEach(func() {
